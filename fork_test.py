579da3c61d0371ec7efdea657cc00de6860e120e
dstch <dstch@163.com>
2019-06-24 17:15:53 +0800

    add new fork code

---
+++ fork_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: dstch
+@license: (C) Copyright 2013-2019, Regulus Tech.
+@contact: dstch@163.com
+@file: fork_test.py
+@time: 2019/6/24 17:15
+@desc:
+"""
+
+# https://www.kaggle.com/chriscc/jigsaw-starter-blend
+# https://www.kaggle.com/cristinasierra/pretext-lstm-tuning-v3
+# https://www.kaggle.com/chechir/bert-lstm-rank-blender
